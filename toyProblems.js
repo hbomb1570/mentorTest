72a8aa5057b4d389f6dc6769a1cfa3922c3a4329
unknown <hbomb1570@yahoo.com>
2018-02-05 17:22:57 -0700

    done?

--- toyProblems.js
+++ toyProblems.js
@@ -41,6 +41,9 @@ module.exports = {
     if(num < 3){
       return false
     }
+    else if (num === 3){
+      return true
+    }
     else for (var i = 2; i < num; i++) {
       if (num%i === 0) {
         return false
